f5d59967e99025a3efe68e4e4eb11aafff35c5c4
Gil Zhaiek <gil@reconinstruments.com>
2014-11-13 09:59:51 -0800

    Added some comments

--- src/com/newcircle/testablecalculator/test/RobotiumCalculator.java
+++ src/com/newcircle/testablecalculator/test/RobotiumCalculator.java
@@ -22,6 +22,10 @@ public class RobotiumCalculator extends BaseTest {
 	public RobotiumCalculator(Solo solo) {
 		mSolo = solo;
 
+		// We use a for loop to search for all the digit buttons.
+		// If we search when we have text inside the buttons (firstNumber or secondNumber),
+		// we will choose the buttons instead of the digits, so we need to do this before we
+		// "corrupt" the view.
 		for(int i = 0; i <= 9; i++) {
 			String iStr = String.valueOf(i);
 			mDigitButtons.put(iStr, mSolo.getButton(iStr));
@@ -30,7 +34,7 @@ public class RobotiumCalculator extends BaseTest {
 	}
 
 	public void calcByEditText(Double x, Double y, OP op, String expectedResults) {
-		mSolo.clearEditText(0);
+		mSolo.clearEditText(0); // The first (index 0) edit box in the view
 		mSolo.enterText(0,  String.valueOf(x) + opToString(op) + String.valueOf(y));
 
 		mSolo.sleep(1000);
@@ -39,28 +43,32 @@ public class RobotiumCalculator extends BaseTest {
 	}
 
 	public void calcByClickingDigits(Double x, Double y, OP op,  String expectedResults) {
+		// We Make sure that the EditBox is clear
 		mSolo.clearEditText(0);
 
-		mSolo.clickOnButton(0);
+		// We now that the first number is also the first button - Index 0
+		mSolo.clickOnButton(0); // 0 is index - not id
+		// We search the CLR button by text
 		mSolo.clickOnButton("CLR");
 		clickThis(x);
 
+		// We now that the second number is also the second button - Index 1
 		mSolo.clickOnButton(1);
 		mSolo.clickOnButton("CLR");
 		clickThis(y);
 
 		switch(op) {
 		case DIV:
-			mSolo.clickOnView(mSolo.getButton(DIV_IDX));
+			mSolo.clickOnView(mSolo.getButton(DIV_IDX));  // We used trial and error to pre-find the index for DIV
 			break;
 		case MINUS:
-			mSolo.clickOnView(mSolo.getButton(MINUS_IDX));
+			mSolo.clickOnView(mSolo.getButton(MINUS_IDX)); // We used trial and error to pre-find the index for MINUS
 			break;
 		case MUL:			
-			mSolo.clickOnView(mSolo.getButton(MUL_IDX));
+			mSolo.clickOnView(mSolo.getButton(MUL_IDX)); // We used trial and error to pre-find the index for MUL
 			break;
 		case PLUS:
-			mSolo.clickOnView(mSolo.getButton(PLUS_IDX));
+			mSolo.clickOnView(mSolo.getButton(PLUS_IDX)); // We used trial and error to pre-find the index for PLUS
 			break;
 		default:
 			break;
